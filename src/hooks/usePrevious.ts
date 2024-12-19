4232bfc214cc68800d476aaee94d5e8c9dd4296e
seungro <seungro@toss.im>
2024-12-19 17:32:26 +0900

    feat. work with new usePrevious

--- src/hooks/usePrevious.ts
+++ src/hooks/usePrevious.ts
@@ -4,36 +4,23 @@ import { useFirstMountState } from './_internal/useFirstMountState.ts';
 
 const strictEquals = <T>(prev: T | undefined, next: T) => prev === next;
 
-type PreviousOptions<T> = {
-  compare?: (prev: T | undefined, next: T) => boolean;
-  withoutCompare?: boolean;
-};
-
 /**
  * Returns the previous value of the input state.
- * If re-rendering occured but value of state is not changed, the previous value will not be updated. If you want to update it, set `options.withoutCompare` to `true`
- * If the state is of object type or if a separate logic is needed to detect changes in value, pass `options.compare` function.
+ * If re-rendering occured but value of state is not changed, the previous value will not be updated.
+ * If the state is of object type or if a customized logic is needed to detect changes in value, pass `compare` function to second parameter.
  * If no compare function is provided, the default comparison `prev === next` will be used.
  *
  * @template T - The type of state.
  * @param {T} state - The state to get previous value.
- * @param {PreviousOptions<T>} options - The options object
- * @param {(prev: T | undefined, next: T) => boolean} options.compare - The comparison function to detect that state has been changed
- * @param {(prev: T | undefined, next: T) => boolean} options.withoutCompare - Set `withoutCompare` to `true` when previous value always need to be updated
+ * @param {(prev: T, next: T) => boolean} compare - The comparison function to detect that state has been changed
  * @returns previous value of the state
  */
-export default function usePrevious<T>(
-  state: T,
-  options: PreviousOptions<T> = {
-    compare: strictEquals,
-    withoutCompare: false,
-  }
-): T | undefined {
-  const prevRef = useRef<T>(undefined);
+export default function usePrevious<T>(state: T, compare: (prev: T, next: T) => boolean = strictEquals): T {
+  const prevRef = useRef<T>(state);
   const currentRef = useRef<T>(state);
   const isFirstMount = useFirstMountState();
 
-  if (isFirstMount || (!options.withoutCompare && options.compare!(currentRef.current, state))) {
+  if (isFirstMount || compare(currentRef.current, state)) {
     return prevRef.current;
   }
 
