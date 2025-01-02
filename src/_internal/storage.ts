c51c7c81e3f3680e4ccb53508869c615f4cebd04
choi2021 <juni2004c@gmail.com>
2025-01-02 23:59:40 +0900

    feat: add safeLocalStorage and safeSessionStorage

--- src/_internal/storage.ts
+++ src/_internal/storage.ts
@@ -27,3 +27,90 @@ export class MemoStorage implements Storage {
     this.storage.clear();
   }
 }
+
+class LocalStorage implements Storage {
+  public static canUse(): boolean {
+    const TEST_KEY = generateTestKey();
+
+    // 사용자가 쿠키 차단을 하는 경우 LocalStorage '접근' 시에 예외가 발생합니다.
+    try {
+      localStorage.setItem(TEST_KEY, 'test');
+      localStorage.removeItem(TEST_KEY);
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  public get(key: string) {
+    return localStorage.getItem(key);
+  }
+
+  public set(key: string, value: string) {
+    localStorage.setItem(key, value);
+  }
+
+  public remove(key: string) {
+    localStorage.removeItem(key);
+  }
+
+  public clear() {
+    localStorage.clear();
+  }
+}
+
+class SessionStorage implements Storage {
+  public static canUse(): boolean {
+    const TEST_KEY = generateTestKey();
+
+    // sessionStorage를 사용할 수 없는 경우에 대응합니다.
+    try {
+      sessionStorage.setItem(TEST_KEY, 'test');
+      sessionStorage.removeItem(TEST_KEY);
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  public get(key: string) {
+    return sessionStorage.getItem(key);
+  }
+
+  public set(key: string, value: string) {
+    sessionStorage.setItem(key, value);
+  }
+
+  public remove(key: string) {
+    sessionStorage.removeItem(key);
+  }
+
+  public clear() {
+    sessionStorage.clear();
+  }
+}
+
+function generateTestKey() {
+  return new Array(4)
+    .fill(null)
+    .map(() => Math.random().toString(36).slice(2))
+    .join('');
+}
+
+export function generateStorage(): Storage {
+  if (LocalStorage.canUse()) {
+    return new LocalStorage();
+  }
+  return new MemoStorage();
+}
+
+export function generateSessionStorage(): Storage {
+  if (SessionStorage.canUse()) {
+    return new SessionStorage();
+  }
+  return new MemoStorage();
+}
+
+export const safeLocalStorage = generateStorage();
+
+export const safeSessionStorage = generateSessionStorage();
