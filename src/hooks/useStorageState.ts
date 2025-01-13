757e303aaf884d6acd73952b9629f61993470ab2
Youngjun Choi <juni2004c@gmail.com>
2025-01-13 20:58:09 +0900

    feat(useStorageState): implement useStorageState (#13)

---
+++ src/hooks/useStorageState.ts
@@ -0,0 +1,102 @@
+import { SetStateAction, useCallback, useRef, useSyncExternalStore } from 'react';
+
+import { safeLocalStorage, Storage } from '../_internal/storage.ts';
+
+type ToPrimitive<T> = T extends string ? string : T extends number ? number : T extends boolean ? boolean : never;
+type ToObject<T> = T extends unknown[] | Record<string, unknown> ? T : never;
+
+export type Serializable<T> = T extends string | number | boolean ? ToPrimitive<T> : ToObject<T>;
+
+interface StorageStateOptions<T> {
+  storage?: Storage;
+  defaultValue?: Serializable<T>;
+}
+
+interface StorageStateOptionsWithDefaultValue<T> extends StorageStateOptions<T> {
+  defaultValue: Serializable<T>;
+}
+
+const listeners = new Set<() => void>();
+
+const emitListeners = () => {
+  listeners.forEach(listener => listener());
+};
+
+/**
+ * a hook that works like useState but stores the state value in the storage and retains the value.
+ * @param key key for storage
+ */
+export function useStorageState<T>(
+  key: string
+): readonly [Serializable<T> | undefined, (value: SetStateAction<Serializable<T> | undefined>) => void];
+export function useStorageState<T>(
+  key: string,
+  { storage, defaultValue }: StorageStateOptionsWithDefaultValue<T>
+): readonly [Serializable<T>, (value: SetStateAction<Serializable<T>>) => void];
+export function useStorageState<T>(
+  key: string,
+  { storage, defaultValue }: StorageStateOptions<T>
+): readonly [Serializable<T> | undefined, (value: SetStateAction<Serializable<T> | undefined>) => void];
+export function useStorageState<T>(
+  key: string,
+  { storage = safeLocalStorage, defaultValue }: StorageStateOptions<T> = {}
+): readonly [Serializable<T> | undefined, (value: SetStateAction<Serializable<T> | undefined>) => void] {
+  const cache = useRef<{
+    data: string | null;
+    parsed: Serializable<T> | undefined;
+  }>({
+    data: null,
+    parsed: defaultValue,
+  });
+
+  const getSnapshot = useCallback(() => {
+    const data = storage.get(key);
+
+    if (data !== cache.current.data) {
+      try {
+        cache.current.parsed = data ? JSON.parse(data) : defaultValue;
+      } catch {
+        cache.current.parsed = defaultValue;
+      }
+      cache.current.data = data;
+    }
+
+    return cache.current.parsed;
+  }, [defaultValue, key, storage]);
+
+  const storageState = useSyncExternalStore<Serializable<T> | undefined>(
+    onStoreChange => {
+      listeners.add(onStoreChange);
+
+      const handler = (event: StorageEvent) => {
+        if (event.key === key) {
+          onStoreChange();
+        }
+      };
+
+      window.addEventListener('storage', handler);
+
+      return () => {
+        listeners.delete(onStoreChange);
+        window.removeEventListener('storage', handler);
+      };
+    },
+    () => getSnapshot()
+  );
+
+  const setStorageState = useCallback(
+    (value: SetStateAction<Serializable<T> | undefined>) => {
+      const nextValue = typeof value === 'function' ? value(getSnapshot()) : value;
+
+      if (nextValue == null) {
+        storage.remove(key);
+      } else {
+        storage.set(key, JSON.stringify(nextValue));
+      }
+      emitListeners();
+    },
+    [getSnapshot, key, storage]
+  );
+
+  return [storageState, setStorageState] as const;
+}
