25844411910bf559cf3450eb73822ca62761b73d
seungro <seungro@toss.im>
2025-04-10 02:15:05 +0900

    fix. fix generate-docs

--- src/hooks/useAsyncEffect/useAsyncEffect.ts
+++ src/hooks/useAsyncEffect/useAsyncEffect.ts
@@ -18,7 +18,7 @@ import { DependencyList, useEffect } from 'react';
  *   return () => {
  *     console.log('Cleanup on unmount or dependency change');
  *   };
- * }, [deps]);
+ * }, [dependency]);
  */
 
 export function useAsyncEffect(effect: () => Promise<void | (() => void)>, deps?: DependencyList) {
