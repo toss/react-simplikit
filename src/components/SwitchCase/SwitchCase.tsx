f94295584da5cdebc167c5c2954641dc6a441c4d
KimCookieYa <min49590@gmail.com>
2025-04-25 00:09:14 +0900

    docs(SwitchCase): add 'boolean' type (#199)
    Co-authored-by: seungrodotlee <seungrodotlee@gmail.com>

--- src/components/SwitchCase/SwitchCase.tsx
+++ src/components/SwitchCase/SwitchCase.tsx
@@ -17,15 +17,15 @@ type Props<Case> = {
  * similar to a `switch-case` statement. It is useful when you need to conditionally render different
  * components depending on a specific state.
  *
- * @param {string | number} value - The value to compare against.
+ * @param {Case} value - The value to compare against.
  *   The component associated with the matching key in `caseBy` will be rendered.
- * @param {Record<string | number, () => JSX.Element>} caseBy - An object that maps values to
+ * @param {Partial<{ [P in StringifiedValue<Case>]: () => ReactElement | null }>} caseBy - An object that maps values to
  *   components to render. The keys represent possible values, and the values are functions returning
  *   the corresponding components.
- * @param {() => JSX.Element} [defaultComponent] - The component to render if `value` does not match
+ * @param {() => ReactElement | null} [defaultComponent] - The component to render if `value` does not match
  *   any key in `caseBy`.
  *
- * @returns {JSX.Element} A React component that conditionally renders based on cases.
+ * @returns {ReactElement | null} A React component that conditionally renders based on cases.
  *
  * @example
  * function App() {
