77c6d2f0dd6cbe44ca55d2f151774c9965d5c296
sukyeong <sukyeong@tossbank.com>
2026-01-14 00:37:08 +0900

    chore: add index.ts

--- packages/mobile/src/index.ts
+++ packages/mobile/src/index.ts
@@ -6,3 +6,4 @@ export { isServer } from './utils/isServer.ts';
 export { getKeyboardHeight } from './utils/keyboard/getKeyboardHeight.ts';
 export { isKeyboardVisible } from './utils/keyboard/isKeyboardVisible.ts';
 export { enableBodyScrollLock, disableBodyScrollLock } from './utils/bodyScrollLock.ts';
+export { isAndroid, isIOS } from './utils/device/device.ts';
