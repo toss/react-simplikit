35049bfe52fefe5d1c642e98a3d3490650776fcd
sukyeong <sukyeong@tossbank.com>
2026-01-14 01:30:02 +0900

    fix: add restoreAllMocks in test code

--- packages/mobile/src/index.ts
+++ packages/mobile/src/index.ts
@@ -2,8 +2,8 @@
 export { useBodyScrollLock } from './hooks/useBodyScrollLock.ts';
 
 // Utils
+export { disableBodyScrollLock, enableBodyScrollLock } from './utils/bodyScrollLock.ts';
 export { isAndroid, isIOS } from './utils/device/device.ts';
 export { isServer } from './utils/isServer.ts';
 export { getKeyboardHeight } from './utils/keyboard/getKeyboardHeight.ts';
 export { isKeyboardVisible } from './utils/keyboard/isKeyboardVisible.ts';
-export { enableBodyScrollLock, disableBodyScrollLock } from './utils/bodyScrollLock.ts';
