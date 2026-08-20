49c419d458e971a32452ff60da33c2d4c324b439
Antoliny0919 <antoliny0919@gmail.com>
2026-08-20 16:55:07 +0900

    test(useInputState): query inputs by role in useInputState spec

--- packages/core/src/hooks/useInputState/useInputState.spec.ts
+++ packages/core/src/hooks/useInputState/useInputState.spec.ts
@@ -11,7 +11,6 @@ function createTestInput(...params: Parameters<typeof useInputState>) {
     const [value, onChange] = useInputState(...params);
 
     return createElement('input', {
-      'data-testid': 'input',
       type: 'text',
       value,
       onChange,
@@ -24,7 +23,6 @@ function createTestTextarea(...params: Parameters<typeof useInputState>) {
     const [value, onChange] = useInputState(...params);
 
     return createElement('textarea', {
-      'data-testid': 'textarea',
       value,
       onChange,
     });
@@ -49,33 +47,33 @@ describe('useInputState', () => {
 });
 
 it('should update value when change event occurs', async () => {
-  const { getByTestId } = render(createElement(createTestInput()));
-  const input = getByTestId('input') as HTMLInputElement;
+  const { getByRole } = render(createElement(createTestInput()));
+  const input = getByRole('textbox');
 
-  expect(input.value).toBe('');
+  expect(input).toHaveValue('');
 
   fireEvent.change(input, { target: { value: 'changed' } });
-  expect(input.value).toBe('changed');
+  expect(input).toHaveValue('changed');
 
   fireEvent.change(input, { target: { value: 'one more changed' } });
-  expect(input.value).toBe('one more changed');
+  expect(input).toHaveValue('one more changed');
 });
 
 it('should transform value according to the provided function', async () => {
-  const { getByTestId } = render(createElement(createTestInput('', v => v.toUpperCase())));
+  const { getByRole } = render(createElement(createTestInput('', v => v.toUpperCase())));
 
-  const input = getByTestId('input') as HTMLInputElement;
+  const input = getByRole('textbox');
   fireEvent.change(input, { target: { value: 'must be uppercase' } });
 
-  expect(input.value).toBe('MUST BE UPPERCASE');
+  expect(input).toHaveValue('MUST BE UPPERCASE');
 });
 
 it('should update value when change event occurs on a textarea', async () => {
-  const { getByTestId } = render(createElement(createTestTextarea()));
-  const textarea = getByTestId('textarea') as HTMLTextAreaElement;
+  const { getByRole } = render(createElement(createTestTextarea()));
+  const textarea = getByRole('textbox');
 
-  expect(textarea.value).toBe('');
+  expect(textarea).toHaveValue('');
 
   fireEvent.change(textarea, { target: { value: 'changed' } });
-  expect(textarea.value).toBe('changed');
+  expect(textarea).toHaveValue('changed');
 });
